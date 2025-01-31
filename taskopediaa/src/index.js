cfafc364f6b6d8c446a75b68c993c5a319fbe71d
Farid Dibirov <farid962@gmail.com>
2025-01-31 17:53:46 +0400

    first react component

--- taskopediaa/src/index.js
+++ taskopediaa/src/index.js
@@ -1,17 +1,45 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+function MainHeader() {
+  return <h1>REACT COURSE</h1>;
+}
+
+function SubHeader() {
+  return <h1>This will be an exciting course.</h1>;
+}
+
+function Header() {
+  return (
+    <div>
+      <MainHeader />
+      <SubHeader />
+    </div>
+  );
+}
+
+function MainBody() {
+  return (
+    <div>
+      <p>In this course, we will learn react js by building TaskOPedia!</p>
+      <ul>
+        <li>Call Ben</li>
+        <li>Go to walmart</li>
+      </ul>
+    </div>
+  );
+}
+
+function Footer() {
+  return <p>Happy Coding!</p>;
+}
+
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+  <div>
+    <Header />
+    <MainBody />
+    <Footer />
+  </div>
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
