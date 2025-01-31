e8cf6b6a23dfea296e2a0c8ce4f135aa55255254
Farid Dibirov <farid962@gmail.com>
2025-01-31 16:56:59 +0400

    taskopedia project created

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
-ReactDOM.render(<p>Hello World from React</p>, document.getElementById("root"));
-ReactDOM.render(<div><h1>React Course</h1><p>We will learn react in this course</p></div>, document.getElementById("root")); 
+// ReactDOM.render(<p>Hello World from React</p>, document.getElementById("root"));
+// ReactDOM.render(<div><h1>React Course</h1><p>We will learn react in this course</p></div>, document.getElementById("root"));
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
+  <div>
+    <h1>React Course</h1>
+    <p>We will learn react in this course</p>
+  </div>
+);
