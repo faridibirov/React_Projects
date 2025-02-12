9888974264925726305014cee6b8b9bd356a6514
Farid Dibirov <farid962@gmail.com>
2025-02-12 19:16:52 +0400

    add contact in action, add validations

--- contactopedia/src/Components/ContactPages/AddContact.jsx
+++ contactopedia/src/Components/ContactPages/AddContact.jsx
@@ -1,34 +1,72 @@
-const AddConact = () => {
-  return (
-    <div className="border col-12 text-white p-2">
-      <div className="row p-2">
-        <div className="col-12 text-white-50">Add a new Contact</div>
-        <div className="col-12 col-md-4 p-1">
-          <input
-            className="form-control form-control-sm"
-            placeholder="Name..."
-          ></input>
-        </div>
-        <div className="col-12 col-md-4 p-1">
-          <input
-            className="form-control form-control-sm"
-            placeholder="Email..."
-          ></input>
-        </div>
-        <div className="col-12 col-md-4 p-1">
-          <input
-            className="form-control form-control-sm"
-            placeholder="Phone..."
-          ></input>
-        </div>
-        <div className="col-12 col-md-6 offset-md-3 p-1">
-          <button className="btn btn-primary btn-sm form-control">
-            Create
-          </button>
-        </div>
+import React from "react";
+
+class AddConact extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      errorMessage: undefined,
+      successMessage: undefined,
+    };
+  }
+
+  handleAddContactFormSubmit = (e) => {
+    e.preventDefault();
+    const name = e.target.elements.contactName.value.trim();
+    const email = e.target.elements.contactEmail.value.trim();
+    const phone = e.target.elements.contactPhone.value.trim();
+    const response = this.props.handleAddContact({
+      name: name,
+      email: email,
+      phone: phone,
+    });
+
+    if (response.status == "success") {
+      this.setState({ errorMessage: undefined, successMessage: response.msg });
+      document.querySelector(".conact-form").reset();
+    } else {
+      this.setState({ errorMessage: response.msg, successMessage: undefined });
+    }
+  };
+  render() {
+    return (
+      <div className="border col-12 text-white p-2">
+        <form
+          onSubmit={this.handleAddContactFormSubmit}
+          className="contact-form"
+        >
+          <div className="row p-2">
+            <div className="col-12 text-white-50">Add a new Contact</div>
+            <div className="col-12 col-md-4 p-1">
+              <input
+                className="form-control form-control-sm"
+                placeholder="Name..."
+                name="contactName"
+              ></input>
+            </div>
+            <div className="col-12 col-md-4 p-1">
+              <input
+                className="form-control form-control-sm"
+                placeholder="Email..."
+                name="contactEmail"
+              ></input>
+            </div>
+            <div className="col-12 col-md-4 p-1">
+              <input
+                className="form-control form-control-sm"
+                placeholder="Phone..."
+                name="contactPhone"
+              ></input>
+            </div>
+            <div className="col-12 col-md-6 offset-md-3 p-1">
+              <button className="btn btn-primary btn-sm form-control">
+                Create
+              </button>
+            </div>
+          </div>
+        </form>
       </div>
-    </div>
-  );
-};
+    );
+  }
+}
 
 export default AddConact;
--- contactopedia/src/Components/ContactPages/ContactIndex.jsx
+++ contactopedia/src/Components/ContactPages/ContactIndex.jsx
@@ -37,6 +37,36 @@ class ContactIndex extends React.Component {
     };
   }
 
+  handleAddContact = (newContact) => {
+    if (newContact.name == "") {
+      return { status: "failure", msg: "Please Enter a valid Name" };
+    } else if (newContact.phone == "") {
+      return { status: "failure", msg: "Please Enter a valid Phone Number" };
+    }
+
+    const duplicateRecord = this.state.contactList.filter((x) => {
+      if (x.name == newContact.name || x.phone == newContact.phone) {
+        return true;
+      }
+    });
+
+    if (duplicateRecord.length > 0) {
+      return { status: "failure", msg: "Duplicate Record" };
+    } else {
+      const newFinalContact = {
+        ...newContact,
+        id: this.state.contactList[this.state.contactList.length - 1].id + 1,
+        isFavorite: false,
+      };
+      this.setState((prevState) => {
+        return {
+          contactList: prevState.contactList.concat([newFinalContact]),
+        };
+      });
+      return { status: "success", msg: "Contact was added successfully" };
+    }
+  };
+
   render() {
     return (
       <div>
@@ -51,7 +81,7 @@ class ContactIndex extends React.Component {
             </div>
             <div className="row py-2">
               <div className="col-8 offset-2 row">
-                <AddConact />
+                <AddConact handleAddContact={this.handleAddContact} />
               </div>
             </div>
             <div className="row py-2">
